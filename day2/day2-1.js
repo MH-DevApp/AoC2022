211e06a89705422e753769aa35721f93f0347cb4
Styx <mehdih.devapp@gmail.com>
2022-12-02 18:40:40 +0100

    feat: AoC day 2

---
+++ day2/day2-1.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+const winPoints = 6;
+const equalPoints = 3;
+const points = {
+  'X': {
+    winsTo: 'C',
+    lostTo: 'B',
+    equalTo: 'A',
+    value: 1
+  },
+  'Y': {
+    winsTo: 'A',
+    lostTo: 'C',
+    equalTo: 'B',
+    value: 2
+  },
+  'Z': {
+    winsTo: 'B',
+    lostTo: 'A',
+    equalTo: 'C',
+    value: 3
+  }
+}
+
+const getScore = (input) => {
+  let score = 0;
+  if (input[0] === points[input[1]].winsTo) {
+    score += winPoints + points[input[1]].value;
+  } else if (input[0] === points[input[1]].equalTo) {
+    score += equalPoints + points[input[1]].value;
+  } else if (input[0] === points[input[1]].lostTo) {
+    score += points[input[1]].value;
+  }
+
+  return score;
+};
+
+let score = 0;
+
+fs
+  .readFileSync('input.txt')
+  .toString()
+  .split('\n')
+  .map((input) => input
+    .split(' ')
+  )
+  .forEach((input) => {
+    score += getScore(input);
+    console.log(score, input);
+  })
+;
+
+console.log(score);
